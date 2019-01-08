5f1834e7ff347ec18922b7ad79c68d503be08689
angelillo97 <angelillo97@gmail.com>
2019-01-08 17:41:04 +0100

    series
    Show series

--- routes/series.js
+++ routes/series.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const serieDriver = require('../lib/datastore/serie-driver');
 
 /* GET users listing. */
-router.get('/', (req, res, next) => {
-	res.render('series', {});
+router.get('/', async function (req, res, next) {
+	const series = await serieDriver.getSeries;
+	res.render('series', {
+		series: series ? series : []
+	});
 });
 
 module.exports = router;

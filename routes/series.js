22f37c90e12213e206b19b2c1c12fd92f73234fd
RodriRR <rodrigorepresa@gmail.com>
2019-01-10 17:07:05 +0100

    comentarios mostrándose

--- routes/series.js
+++ routes/series.js
@@ -2,13 +2,16 @@ const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middlewares/auth');
 const serieDriver = require('../lib/datastore/serie-driver');
+const comentarioDriver = require('../lib/datastore/comentarios-driver');
+
 
 router.use(authMiddleware);
 
 
 /* GET users listing. */
-router.get('/', async function (req, res, next) {
+router.get('/', async (req, res, next) => {
 	const series = await serieDriver.getSeries();
+
 	res.render('series', {
 		series: series ? series : []
 	});
@@ -18,6 +21,20 @@ router.get('/', async function (req, res, next) {
 /* GET users listing. */
 router.get('/add', (req, res, next) => {
 	res.render('addSerie', {});
+});
+
+/* GET users listing. */
+router.get('/comentarios', async (req, res, next) => {
+	// Obtengo la ide de la serie
+	const id = req.query.id;
+	// Obtengo la id de los comentarios de la serie
+	const idComentarios = await serieDriver.getidComentarios(id);
+	// Obtengo los comentarios
+	const comentarios = await comentarioDriver.getComentarios(idComentarios);
+
+	res.render('comentarios', {
+		comentarios: comentarios ? comentarios : []
+	});
 });
 
 

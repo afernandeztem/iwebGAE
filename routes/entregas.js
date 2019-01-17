ae69900380e0242c5a2cd16847e25217ddc8bdce
FRANCISCO REYES SANCHEZ <6001174@insags.com>
2019-01-17 13:46:09 +0100

    Mostrar entregas y eliminar entregas implemetnados pero no funciona

--- routes/entregas.js
+++ routes/entregas.js
@@ -6,14 +6,18 @@ const authMiddleware = require('../middlewares/auth');
 router.use(authMiddleware);
 
 /* GET users listing. */
-router.get('/', async (req, res, next) => {
+router.get('/', async(req, res, next) => {
 
 	//obtengo el usuario de la sesion
-	const data = req.session.passport;
+    const data = req.session.passport;
 	const email = data.profile.emails[0].value;
-	const emailUser = data.profile.emails[0].value;
-
 	const misEntregas = await entregaDriver.getEntregasUsuario(email);
+	
+	const emailUser = data.profile.emails[0].value;
+	console.log(emailUser);
+	
+
+	console.log(misEntregas);
 
 	res.render('entregas', {
 		entregas: misEntregas ? misEntregas : [],
@@ -44,17 +48,19 @@ router.get('/eliminar', async (req, res, next) => {
 	const id = req.query.id;
 	// Obtengo el email del usuario que ha creado la serie
 	const emailEntrega = await entregaDriver.getUsuarioEntrega(id);
-
+	
 	const data = req.session.passport;
-	console.log(data);
+	
 	const email = data.profile.emails[0].value;
 	if (emailEntrega === email) {
 		console.log('PERMISO PARA ELIMINAR ENTREGA');
 		await entregaDriver.deleteEntrega(id);
 	}
 
-
-	res.redirect('/entregas');
+	//obtengo el id de la serie con el id de la entrega
+	const idSerie = await entregaDriver.getSerieEntrega(id);
+	
+	res.redirect('/series/entregas?id='+ idSerie);
 });
 
 module.exports = router;
